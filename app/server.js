9c0caf88c057ed3be478214ea240f4e3a526f2f8
aracytopterm <almeidaleonardor@gmail.com>
2017-06-22 18:14:15 -0300

    char adicionado, valores ainda dummy e nao passados corretamente para pagina

--- app/server.js
+++ app/server.js
@@ -5,11 +5,16 @@ var path = require("path");
 
 app.use(express.static("public"));
 
+app.use("/chart", express.static("./../node_modules/chart.js/dist/"));
+
 app.post('/ar_mf', function (req, res) {
     req.on('data', function (data) {
             console.log(data.toString());
     });
     controller.exeCmd("bash ./sh_runAnalysis/sh_runAnalysis.sh");
+
+    //response
+    res.end("bla");
 });
 
 
