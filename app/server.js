b069fdb1916522bab5a0bc6a269a5b8111fe3ccb
aracytopterm <almeidaleonardor@gmail.com>
2017-06-25 19:45:33 -0300

    arquivo local jquery adicionado; formulario enviando dados para o servidos; falta ajustar o script para usar esses dados;

--- app/server.js
+++ app/server.js
@@ -1,20 +1,27 @@
 const controller = require("./controller");
 const express = require('express');
-const app = express();
 var path = require("path");
+var bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
 app.use("/chart", express.static("./../node_modules/chart.js/dist/"));
+app.use("/jquery", express.static("./../node_modules/jquery/dist/"));
 
 app.post('/ar_mf', function (req, res) {
-    req.on('data', function (data) {
-            console.log(data.toString());
-    });
+
+    //values sended from client forms
+    console.log(req.body);
+
+    //executa script shell e recebe o valor
     controller.exeCmd("bash ./sh_runAnalysis/sh_runAnalysis.sh");
 
     //response
-
     var JSON_response = [];
     for(var i = 0; i < 8; i++) {
         var JSON_point = new Object();
@@ -22,13 +29,14 @@ app.post('/ar_mf', function (req, res) {
         JSON_point.y= i;
         JSON_response.push(JSON_point);
     }
-
+    
     console.log(JSON_response);
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify(JSON_response));
+
 });
 
 
-app.listen(3000, function () {
-    console.log("Server running on 3000 port");
+app.listen(5000, function () {
+    console.log("Server running on 5000 port");
 });
